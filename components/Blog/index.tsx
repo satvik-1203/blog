86e2d14ec909dc7ffeed727ae79833afef8d26f2
satvik-1203 <satvikgsu@gmail.com>
2021-07-25 12:40:27 +0530

    life cycles

--- components/Blog/index.tsx
+++ components/Blog/index.tsx
@@ -26,6 +26,7 @@ const index: React.FC<Props> = ({ data, content }) => {
         h3 {
           font-size: 1.5rem;
           font-weight: bold;
+          margin: 1rem 0;
         }
         h2 {
           font-size: 2.2rem;
@@ -38,8 +39,9 @@ const index: React.FC<Props> = ({ data, content }) => {
         pre {
           overflow-x: scroll;
           background: #1d1d1d;
-          padding: 0.6rem 0.6rem;
+          padding: 0.9rem 0.8rem;
           border-radius: 0.5rem;
+          font-size: 0.8rem;
         }
         code {
           background: rgb(255, 255, 255, 0.7);
@@ -48,6 +50,16 @@ const index: React.FC<Props> = ({ data, content }) => {
         pre code {
           background: transparent;
           color: white;
+        }
+        blockquote {
+          border-radius: 0.4rem;
+
+          padding-left: 0.6rem;
+          background-color: white;
+        }
+        blockquote p {
+          padding: 0.3rem 0.4rem;
+          background-color: rgba(167, 167, 167, 0.6);
         }
       `}</style>
     </>
