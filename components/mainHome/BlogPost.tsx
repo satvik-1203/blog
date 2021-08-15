9e4629e48eaa0bc85558a95a7c605ab0cf918cc9
satvik-1203 <satvikgsu@gmail.com>
2021-08-15 05:46:23 +0530

    changes to the blog

--- components/mainHome/BlogPost.tsx
+++ components/mainHome/BlogPost.tsx
@@ -7,8 +7,8 @@ interface Props {
 
 const BlogPost: React.FC<Props> = ({ topicName }) => {
   return (
-    <div className="p-4  cursor-pointer  h-[11rem] flex flex-col justify-between  transition-all shadow-md hover:shadow-lg transform hover:-translate-y-3 rounded-lg bg-card ">
-      <div className="text-lg"> {topicName.name}</div>
+    <div className="p-4  relative cursor-pointer  h-[11rem] flex flex-col justify-between  transition-all shadow-md hover:shadow-lg transform hover:-translate-y-3 rounded-lg bg-card ">
+      <div className="text-lg font-bold"> {topicName.name}</div>
       <div className="text-sm font-mono">{topicName.language}</div>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,9 @@ import Head from "next/head";
 import { InferGetStaticPropsType } from "next";
 import fs from "fs";
 import blogs from "../blogs/blogs.json";
-import MainHome from "../components/mainHome/index";
-import { topicName } from "interfaces";
+import MainHome from "@Components/mainHome/index";
 
 export default function Home({
-  topicNames,
   blogs,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
@@ -24,16 +22,16 @@ export default function Home({
 export const getStaticProps = async () => {
   const files = fs.readdirSync("blogs/content", "utf-8");
 
-  const topicNames: topicName[] = files.map((file, index) => ({
-    id: index + 1,
-    name: file
-      .replace(".mdx", "")
-      .split("-")
-      .map(word => word[0].toUpperCase() + word.slice(1))
-      .join(" "),
-  }));
+  // const topicNames: topicName[] = files.map((file, index) => ({
+  //   id: index + 1,
+  //   name: file
+  //     .replace(".mdx", "")
+  //     .split("-")
+  //     .map(word => word[0].toUpperCase() + word.slice(1))
+  //     .join(" "),
+  // }));
 
   return {
-    props: { topicNames, blogs },
+    props: { blogs },
   };
 };
