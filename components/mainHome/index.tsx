f3634ae58231dcc52ba1c9c87293e3b08df91391
satvik-1203 <satvikgsu@gmail.com>
2021-09-01 22:17:01 -0400

    clean up of all blogs, will be starting fresh :)

--- components/mainHome/index.tsx
+++ components/mainHome/index.tsx
@@ -12,12 +12,11 @@ const index: React.FC<Props> = ({ topicNames }) => {
   return (
     <main>
       <header>
-        <h1 className="font-serif py-[4rem] text-center text-2xl px-5 md:text-3xl lg:text-4xl 2xl:text-6xl">
-          This is a place where I share the world what I find amazing in web
-          development
+        <h1 className="font-serif py-[4rem] text-center text-2xl px-5 md:text-3xl lg:text-5xl 2xl:text-6xl">
+          {"((:"} Everything I find amazing can be found here {":))"}
         </h1>
       </header>
-      <section className=" pb-[5rem] grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-10 px-5">
+      <section className=" pb-[5rem] grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-10 px-5">
         {topicNames.map(topicName => (
           <Link key={topicName.id} href={topicName.url || "/"}>
             <a>
--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home({
           name="description"
           content="Place where you can read everything satvik's wants to share :). Thanks for being here"
         />
-        <link rel="icon" href="/fav.svg" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <MainHome topicNames={blogs} />
     </div>
